64ac506c09b3f555352d236501b0248e7c965029
osoken <osoken.devel@outlook.jp>
2017-12-15 20:47:15 +0900

    add game route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/game/:id', function(req, res, next) {
+  res.render('game', { gameid: req.params.id});
 });
 
 module.exports = router;
