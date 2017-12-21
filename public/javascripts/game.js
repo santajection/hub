c2823b7be963afd8ae3e08c1f971eed30dbe603c
osoken <osoken.devel@outlook.jp>
2017-12-21 20:44:01 +0900

    implement join

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,11 +1,15 @@
-!((function(d3, io, Shake) {
+!(function(d3, io, Shake) {
   var socket = io.connect('/mobile');
 
   socket.on('connect', function() {
   });
 
   function initGame() {
-    socket.emit('join', {name: 'debugger', color: 'red'});
+    d3.select('#button_join').on('click', function() {
+      d3.event.preventDefault();
+      var name = d3.select('#input_name').property('value');
+      socket.emit('join', {name: name});
+    });
     var shake = new Shake({
       threshold: 15,
       timeout: 10
@@ -13,12 +17,10 @@
     shake.start();
     window.addEventListener('shake', shakeEventDidOccur, false);
 
-    //function to call when shake occurs
     function shakeEventDidOccur () {
       d3.select('body').insert('div', 'div').text(new Date().getTime());
       socket.emit('move', 1);
     }
-  };
-
-  initGame();
-})(d3, io, Shake));
+  }
+  this.initGame = initGame;
+})(d3, io, Shake);
